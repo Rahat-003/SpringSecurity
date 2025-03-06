f8bcf83b0da1765ab51f47e35293412a306d7aba
rahat <rahatakhter4@gmail.com>
2025-03-06 11:11:32 +0600

    csrf token

---
+++ src/main/java/com/example/Spring_Security_Demo/StudentController.java
@@ -0,0 +1,39 @@
+package com.example.Spring_Security_Demo;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "Rahim", "Java"),
+            new Student(2, "Karim", "C++")
+    ));
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @PostMapping("students")
+    public void addStudent(@RequestBody Student student) {
+            System.out.println(student);
+        students.add(student);
+    }
+
+
+}
