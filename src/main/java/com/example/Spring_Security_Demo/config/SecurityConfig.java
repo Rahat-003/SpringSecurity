6f6b1faaca2f602e5a7b0dd283a292cb51355641
rahat <rahatakhter4@gmail.com>
2025-03-09 15:55:40 +0600

    without lambda

--- src/main/java/com/example/Spring_Security_Demo/config/SecurityConfig.java
+++ src/main/java/com/example/Spring_Security_Demo/config/SecurityConfig.java
@@ -1,26 +1,33 @@
 package com.example.Spring_Security_Demo.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfToken;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(customizer -> customizer.disable());
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-//        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
 
-        // when session is stateless, form login can be disabled
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        return http.build();
+        Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> configurer) {
+                configurer.disable();
+            }
+        };
+
+        http.csrf(custCsrf);
+
+//        return http.build();
     }
 }
