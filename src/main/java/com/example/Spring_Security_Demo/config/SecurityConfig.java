5dfec28fa2081704e528c142e1a32005978c8cf6
rahat <rahatakhter4@gmail.com>
2025-03-10 12:59:57 +0600

    login using user or admin

--- src/main/java/com/example/Spring_Security_Demo/config/SecurityConfig.java
+++ src/main/java/com/example/Spring_Security_Demo/config/SecurityConfig.java
@@ -8,7 +8,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CsrfToken;
 
@@ -16,9 +19,9 @@ import org.springframework.security.web.csrf.CsrfToken;
 @EnableWebSecurity
 public class SecurityConfig {
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
 
         http
                 .csrf(customizer -> customizer.disable())
@@ -33,7 +36,20 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
 
-        return null;
+        UserDetails user = User
+                .withDefaultPasswordEncoder()
+                .username("user")
+                .password("1234")
+                .roles("USER").build();
+
+        UserDetails admin = User
+                .withDefaultPasswordEncoder()
+                .username("admin")
+                .password("1234")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
